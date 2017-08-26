6774bd7221541e1676f0d7bb4a9d274ad8634842
Robert Rock <rrock@globalfit.com>
2017-08-25 20:40:40 -0400

    develop
    Added Pages, clock and profile. Added Providers util and auth ... bare templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { UtilServiceProvider } from '../providers/util-service/util-service';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UtilServiceProvider,
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
