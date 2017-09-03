28e58370ce1e70d931f079889520775dba8e351f
Robert Rock <brock@8thwonder.io>
2017-09-03 09:57:16 -0400

    gulp jsdoc and gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var jsdoc = require('gulp-jsdoc3');
 
+/**
+ * Default Gulp Task
+ */
 gulp.task('default', function() {
-  // Place code for Default task
+  return gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
 
+/**
+ * Documentation Task
+ *
+ * Output: docs/gen -- Location set in jsdoc/src/jsdocConfig.json
+ */
+gulp.task('docs', function(callback) {
+  gulp.src(['README.md', './src/**/*.js'], {read: false})
+    .pipe(jsdoc(callback));
 });
