3ec1d5bc3e47020a3fd534d1ebac6b5182d2a1a6
Robert Rock <brock@8thwonder.io>
2017-09-04 17:48:22 -0400

    update to gulp file adding run-sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,31 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var jsdoc = require('gulp-jsdoc3');
-
-/**
- * Default Gulp Task
- */
-gulp.task('default', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat('all.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
-});
+let gulp = require('gulp'),
+  babel = require('gulp-babel'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  jsdoc = require('gulp-jsdoc3');
 
 /**
  * Documentation Task
  *
  * Output: docs/gen -- Location set in jsdoc/src/jsdocConfig.json
+ *  Can be set with custom config file
  */
 gulp.task('docs', function(callback) {
   gulp.src(['README.md', './src/**/*.js'], {read: false})
     .pipe(jsdoc(callback));
+});
+
+/**
+ * Default Gulp Task
+ */
+gulp.task('default', function() {
+  let runSequence = require('run-sequence');
+
+  runSequence('docs');
+  // return gulp.src('src/**/*.js')
+  //   .pipe(sourcemaps.init())
+  //   .pipe(babel())
+  //   .pipe(concat('all.js'))
+  //   .pipe(sourcemaps.write('.'))
+  //   .pipe(gulp.dest('dist'));
 });
